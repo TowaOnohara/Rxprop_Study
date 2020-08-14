5aec7cd36ffb772b89afe7208464cf721226d1ee
yasuo okamoto <jack.jilljp2@gmail.com>
2020-08-14 09:45:00 +0900

    バインド方式による違いを示すプロパティをVMに定義

---
+++ Rxprop_Study/Models/POCO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rxprop_Study.Models
+{
+    public class POCO: BindableBase
+    {
+        private string _FirstName;
+        public string FirstName 
+        {
+            get { return _FirstName; }
+            set { SetProperty(ref _FirstName, value); }
+        }
+
+        private string _LastName;
+        public string LastName 
+        {
+            get { return _LastName; }
+            set { SetProperty(ref _LastName, value); }
+        }
+    }
+}
--- Rxprop_Study/ViewModels/CreateFromPocoWindowViewModel.cs
+++ Rxprop_Study/ViewModels/CreateFromPocoWindowViewModel.cs
@@ -1,11 +1,49 @@
-﻿using Rxprop_Study.Models;
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using Rxprop_Study.Models;
 using System;
 using System.Collections.Generic;
+using System.Reactive.Disposables;
 using System.Text;
 
 namespace Rxprop_Study.ViewModels
 {
-    public class CreateFromPocoWindowViewModel : BindableBase
+    public class CreateFromPocoWindowViewModel : ViewModelBase
     {
+        public POCO Poco { get; } = new POCO
+        {
+            FirstName = "yasuo",
+            LastName = "okamoto",
+        };
+        public ReactiveProperty<string> FirstNameTwoWay { get; }
+        public ReactiveProperty<string> LastNameTwoWay { get; }
+        public ReadOnlyReactiveProperty<string> FirstNameOneWay { get; }
+        public ReadOnlyReactiveProperty<string> LastNameOneWay { get; }
+        public ReactiveProperty<string> FirstNameToSource { get; }
+        public ReactiveProperty<string> LastNameToSource { get; }
+
+        public CreateFromPocoWindowViewModel() 
+        {
+            // バインド方式:TwoWay 
+            FirstNameTwoWay = Poco.ToReactivePropertyAsSynchronized(x => x.FirstName)
+                .AddTo(Disposables);
+            LastNameTwoWay = Poco.ToReactivePropertyAsSynchronized(x => x.LastName)
+                .AddTo(Disposables);
+
+            // バインド方式:OneWay
+            FirstNameOneWay = Poco.ObserveProperty(x => x.FirstName)
+                .ToReadOnlyReactiveProperty()
+                .AddTo(Disposables);
+            LastNameOneWay = Poco.ObserveProperty(x => x.LastName)
+                .ToReadOnlyReactiveProperty()
+                .AddTo(Disposables);
+
+            // バインド方式:OneWayToSource
+            FirstNameToSource = ReactiveProperty.FromObject(Poco, x => x.FirstName);
+                //.AddTo(Disposables);  // TODO:不要？
+            LastNameToSource = ReactiveProperty.FromObject(Poco, x => x.LastName);
+                //.AddTo(Disposables);  // TODO:不要？
+
+        }
     }
 }
