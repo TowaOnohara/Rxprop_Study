5e189311c7d1465cfc07c02b79a321921cd35332
yasuo okamoto <jack.jilljp2@gmail.com>
2020-08-17 22:40:43 +0900

    画面コントロールを配置

--- Rxprop_Study/ViewModels/ValidationWindowViewModel.cs
+++ Rxprop_Study/ViewModels/ValidationWindowViewModel.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Rxprop_Study.ViewModels
 {
     public class ValidationWindowViewModel: ViewModelBase
     {
+        [Required(ErrorMessage = "Required property")]
+        public ReactiveProperty<string> WithDataAnnotations { get; }
+
     }
 }
