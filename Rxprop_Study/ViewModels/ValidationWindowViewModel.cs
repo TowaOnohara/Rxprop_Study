7a7f18cb0033a1756e4e44833f76dec9989c600b
yasuo okamoto <jack.jilljp2@gmail.com>
2020-08-21 00:26:51 +0900

    属性について改めて調べた。基本的なことやけど。。。

--- Rxprop_Study/ViewModels/ValidationWindowViewModel.cs
+++ Rxprop_Study/ViewModels/ValidationWindowViewModel.cs
@@ -8,8 +8,15 @@ namespace Rxprop_Study.ViewModels
 {
     public class ValidationWindowViewModel: ViewModelBase
     {
-        [Required(ErrorMessage = "Required property")]
+        [Required(ErrorMessage = "Required property")]  // 値が格納されている必要がある、という属性(※)
         public ReactiveProperty<string> WithDataAnnotations { get; }
 
     }
+
+    // ※属性
+    // C#の機能。クラスや変数に対して負荷するもので、コンパイラやユーザー(プログラマ)に
+    // 振る舞いを伝えることができる。
+    // publicやprivateなども一種の属性。こういったものを自作できる機能。
+    //https://ufcpp.net/study/csharp/sp_attribute.html
+
 }
