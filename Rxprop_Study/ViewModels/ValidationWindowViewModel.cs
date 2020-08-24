4abfc9fa93bedf5727d9ca451b9305c5b37da18c
yasuo okamoto <jack.jilljp2@gmail.com>
2020-08-25 00:00:58 +0900

    コマンド処理を追加

--- Rxprop_Study/ViewModels/ValidationWindowViewModel.cs
+++ Rxprop_Study/ViewModels/ValidationWindowViewModel.cs
@@ -4,6 +4,10 @@ using Rxprop_Study.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Rxprop_Study.ViewModels
@@ -18,6 +22,7 @@ namespace Rxprop_Study.ViewModels
         public ReadOnlyReactivePropertySlim<string> WithCustomValidationLogicErrMessage { get; }
 
         public ReactiveProperty<bool> HasValidationErrors { get; }
+        public ReactiveCommand SubmitCommand { get; }
 
         //[Required(ErrorMessage = "Required property2")]  // 値が格納されている必要がある、という属性(※)
         //public ReactiveProperty<string> WithDataAnnotations2 { get; }
@@ -25,9 +30,9 @@ namespace Rxprop_Study.ViewModels
 
         public POCO poco { get; } = new POCO { LastName = "test", FirstName = "TEST" };
 
-        public ValidationWindowViewModel() 
+        public ValidationWindowViewModel()
         {
-            
+
             WithDataAnnotations = new ReactiveProperty<string>()    // 1) ReavtivePropertyを生成する
                 .SetValidateAttribute(() => WithDataAnnotations)    // 2) WithDataAnnotationsに割り当たっている属性を1)で作ったインスタンスにセットする
                 .AddTo(Disposables);                                // 3) まとめてDisposeできるように登録
@@ -45,6 +50,33 @@ namespace Rxprop_Study.ViewModels
                 .ObserveValidationErrorMessage()
                 .ToReadOnlyReactivePropertySlim()
                 .AddTo(Disposables);
+
+            HasValidationErrors = new[]
+            {
+                WithDataAnnotations.ObserveHasErrors,
+                WithCustomValidationLogic.ObserveHasErrors,
+            }.CombineLatest(x => x.Any(y => y))
+            .ToReactiveProperty()
+            .AddTo(Disposables);
+
+            //HasValidationErrors = WithDataAnnotations.ObserveHasErrors
+            //    .CombineLatest(WithCustomValidationLogic.ObserveHasErrors, (x, y) => x | y)
+            //    .ToReactiveProperty()
+            //    .AddTo(Disposables);
+            ////https://blog.okazuki.jp/entry/20120219/1329663635
+
+
+            SubmitCommand = new[]
+            {
+                WithDataAnnotations.ObserveHasErrors.Inverse(),         // WithDataAnnotationsにエラーがないこと
+                WithCustomValidationLogic.ObserveHasErrors.Inverse(),   // WithCustomValidationLogicにエラーがないこと
+            }.CombineLatestValuesAreAllTrue()                           // どちらかのイベントが発行されたときに、どちらもTrueであること
+            .ToReactiveCommand()                                        // コマンドに変更
+            .AddTo(Disposables);
+
+            // コマンド処理処理の登録
+            SubmitCommand.Subscribe(_ => Trace.WriteLine("SubmitCommand"));
+
 
 
             // TODO: ModelをSyncしているときはどのような挙動になるのか？
